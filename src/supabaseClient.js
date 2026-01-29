c71ebb44986f93a6f21d243b2c626255063d69d7
thub-app <elitefitnessbg@gmail.com>
2026-01-29 12:06:51 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://xyohzannkzziolhftayu.supabase.co'
-const supabaseAnonKey = 'sb_publishable_by1vBM0ddFkyykzmjw_Hrw_NSin9VpR'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5b2h6YW5ua3p6aW9saGZ0YXl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2NzQ1MDcsImV4cCI6MjA4NTI1MDUwN30.HAdTs9sFlplcqSbUQUwCLGWRY9Te8sV_m271FkVqsh4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
